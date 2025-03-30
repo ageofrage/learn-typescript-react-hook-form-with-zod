78f6b0de73d21cf97d9af4db60131a62264fe1d1
ageofrage <agehara.dev@gmail.com>
2025-03-31 01:44:20 +0900

    refact: component dir

---
+++ src/components/Pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Form } from "./components/Form"
+
+export const Main = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Form />
+    </div>
+  )
+}
